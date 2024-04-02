66e3980b233076dde304e871be71d80551aefb43
wellsiau-aws <wellsiau@amazon.com>
2024-04-01 21:15:53 -0700

    migrate to pre-entrypoint helper

--- .project_automation/functional_tests/entrypoint.sh
+++ .project_automation/functional_tests/entrypoint.sh
@@ -43,45 +43,8 @@ else
     EXIT_CODE=1
 fi
 
-<<<<<<< before updating
-#********** TFC Env Vars *************
-export AWS_DEFAULT_REGION=us-east-1
-export TFE_TOKEN=`aws secretsmanager get-secret-value --secret-id abp/tfc/token | jq -r ".SecretString"`
-export TF_TOKEN_app_terraform_io=`aws secretsmanager get-secret-value --secret-id abp/tfc/token | jq -r ".SecretString"`
-
-#********** MAKEFILE *************
-echo "Build the lambda function packages"
-make all
-
-#********** Get tfvars from SSM *************
-echo "Get *.tfvars from SSM parameter"
-aws ssm get-parameter \
-  --name "/abp/tfc/functional/tfvars" \
-  --with-decryption \
-  --query "Parameter.Value" \
-  --output "text" \
-  --region "us-east-1" >> functional_test.tfvars
-
-#********** Terratest execution **********
-echo "Running Terratest"
-export GOPROXY=https://goproxy.io,direct
-cd test
-rm -f go.mod
-go mod init github.com/aws-ia/terraform-project-ephemeral
-go mod tidy
-go install github.com/gruntwork-io/terratest/modules/terraform
-go test -timeout 45m
-
-#********** CLEANUP *************
-echo "Cleaning up all temp files and artifacts"
-cd ${PROJECT_PATH}
-make clean
-
-echo "End of Functional Tests"
-=======
 #********** Post-entrypoint helper *************
 post_entrypoint
 
 #********** Exit Code *************
 exit $EXIT_CODE
->>>>>>> after updating
