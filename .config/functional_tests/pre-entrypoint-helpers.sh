f755bcf0ae73909209226c478a1d68c03ec75c35
wellsiau-aws <wellsiau@amazon.com>
2024-04-09 10:00:07 -0700

    add entrypoint helper

--- .config/functional_tests/pre-entrypoint-helpers.sh
+++ .config/functional_tests/pre-entrypoint-helpers.sh
@@ -4,3 +4,26 @@
 ## TIPS: avoid modifying the .project_automation/functional_test/entrypoint.sh
 ## migrate any customization you did on entrypoint.sh to this helper script
 echo "Executing Pre-Entrypoint Helpers"
+
+#********** Project Path *************
+PROJECT_PATH=${BASE_PATH}/project
+PROJECT_TYPE_PATH=${BASE_PATH}/projecttype
+cd ${PROJECT_PATH}
+
+#********** TFC Env Vars *************
+export AWS_DEFAULT_REGION=us-east-1
+export TFE_TOKEN=`aws secretsmanager get-secret-value --secret-id abp/tfc/token | jq -r ".SecretString"`
+export TF_TOKEN_app_terraform_io=`aws secretsmanager get-secret-value --secret-id abp/tfc/token | jq -r ".SecretString"`
+
+#********** MAKEFILE *************
+echo "Build the lambda function packages"
+make all
+
+#********** Get tfvars from SSM *************
+echo "Get *.tfvars from SSM parameter"
+aws ssm get-parameter \
+  --name "/abp/tfc/functional/tfc_org/terraform_test.tfvars" \
+  --with-decryption \
+  --query "Parameter.Value" \
+  --output "text" \
+  --region "us-east-1" >> ./tests/terraform.auto.tfvars
