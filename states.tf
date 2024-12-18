d5d4df743cfd76ca0dc03adfaa021f7b89cf73e2
wellsiau-aws <wellsiau@amazon.com>
2024-12-17 21:15:42 -0800

    use vendedlogs namespace for SFN log

--- states.tf
+++ states.tf
@@ -20,7 +20,7 @@ resource "aws_sfn_state_machine" "runtask_states" {
 }
 
 resource "aws_cloudwatch_log_group" "runtask_states" {
-  name              = "/aws/state/${var.name_prefix}-runtask-statemachine"
+  name              = "/aws/vendedlogs/states/${var.name_prefix}-runtask-statemachine"
   retention_in_days = var.cloudwatch_log_group_retention
   kms_key_id        = aws_kms_key.runtask_key.arn
   tags              = var.tags
